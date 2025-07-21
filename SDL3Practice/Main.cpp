b57bc7fe11a8a6e64d4ab60c37e25124fe210154
INTONIGHT <tyler.throop@gmail.com>
2025-07-21 13:28:53 -0700

    creating the foundation

--- SDL3Practice/Main.cpp
+++ SDL3Practice/Main.cpp
@@ -3,25 +3,41 @@
 //this sdl main is needed for the sdl to do its thing
 using namespace std;
 
-void cleanup(SDL_Window *win);
+struct SDLState {
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+};
+
+void cleanup(SDLState &state);
 
 
 
 int main(int argc, char* argv[]) {
 //it needs this argc and argv as well as its pulling it from the command line
 //NB You can only have one main file in your project like this otherwise it gets a little confused :)
+	SDLState state;
 	if (!SDL_Init(SDL_INIT_VIDEO)) {
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", nullptr);
+		cleanup(state);
 		return 1;
 	}
 	//create the window
 	int width = 600;
 	int height = 600;
-	SDL_Window *win = SDL_CreateWindow("SDL3 practice", width, height, 0);
-	if (!win) {
-		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", win);
+	state.window = SDL_CreateWindow("SDL3 practice", width, height, 0);
+	if (!state.window) {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", state.window);
+		cleanup(state);
 		return 1;
 	}
+	//create the Renderer
+	state.renderer = SDL_CreateRenderer(state.window, nullptr);
+	if (!state.renderer) {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error Creating Renderer", state.window);
+		cleanup(state);
+		return 1;
+	}
+
 	//start the game loop
 	bool running = true;
 	while (running) {
@@ -34,11 +50,18 @@ int main(int argc, char* argv[]) {
 				break;
 			}
 		}
+		//perform drawing
+		//starting simple this will draw the background white
+		SDL_SetRenderDrawColor(state.renderer, 255, 255, 255, 255);
+		SDL_RenderClear(state.renderer);
+		//swap bufgfer
+		SDL_RenderPresent(state.renderer);
 	}
-	cleanup(win);
+	cleanup(state);
 	return 0;
 }
-void cleanup(SDL_Window *win) {
-	SDL_DestroyWindow(win);
+void cleanup(SDLState &state) {
+	SDL_DestroyRenderer(state.renderer);
+	SDL_DestroyWindow(state.window);
 	SDL_Quit();
 }
