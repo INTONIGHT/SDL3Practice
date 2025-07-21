7660eefa259dfb392323c3acb70f941b9fb240c4
INTONIGHT <tyler.throop@gmail.com>
2025-07-21 13:19:08 -0700

    starting to build out the logic that will be a basis i nuked sample but you can find it just by researching sdl3 evample code

--- SDL3Practice/Main.cpp
+++ SDL3Practice/Main.cpp
@@ -1,52 +1,44 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+//this sdl main is needed for the sdl to do its thing
+using namespace std;
 
-int main(int argc, char* argv[])
-{
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    SDL_Surface* surface;
-    SDL_Texture* texture;
-    SDL_Event event;
+void cleanup(SDL_Window *win);
 
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
-        return 3;
-    }
 
-    if (!SDL_CreateWindowAndRenderer("Hello SDL", 320, 240, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
-        return 3;
-    }
 
-    surface = SDL_LoadBMP("sample.bmp");
-    if (!surface) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", SDL_GetError());
-        return 3;
-    }
-    texture = SDL_CreateTextureFromSurface(renderer, surface);
-    if (!texture) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
-        return 3;
-    }
-    SDL_DestroySurface(surface);
-
-    while (1) {
-        SDL_PollEvent(&event);
-        if (event.type == SDL_EVENT_QUIT) {
-            break;
-        }
-        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
-        SDL_RenderClear(renderer);
-        SDL_RenderTexture(renderer, texture, NULL, NULL);
-        SDL_RenderPresent(renderer);
-    }
-
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-
-    SDL_Quit();
-
-    return 0;
+int main(int argc, char* argv[]) {
+//it needs this argc and argv as well as its pulling it from the command line
+//NB You can only have one main file in your project like this otherwise it gets a little confused :)
+	if (!SDL_Init(SDL_INIT_VIDEO)) {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", nullptr);
+		return 1;
+	}
+	//create the window
+	int width = 600;
+	int height = 600;
+	SDL_Window *win = SDL_CreateWindow("SDL3 practice", width, height, 0);
+	if (!win) {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", win);
+		return 1;
+	}
+	//start the game loop
+	bool running = true;
+	while (running) {
+		SDL_Event event{ 0 };
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+				//for when the user wants to quit out themselves
+			case SDL_EVENT_QUIT:
+				running = false;
+				break;
+			}
+		}
+	}
+	cleanup(win);
+	return 0;
+}
+void cleanup(SDL_Window *win) {
+	SDL_DestroyWindow(win);
+	SDL_Quit();
 }
