49f619b583963bba68284de8f410054938b4ccf5
INTONIGHT <tyler.throop@gmail.com>
2025-07-23 12:27:27 -0700

    I figured out why it wasnt rendering it correctly and got a better sprite as an example im not an artist so borrowing someone elses drawing

--- SDL3Practice/Main.cpp
+++ SDL3Practice/Main.cpp
@@ -8,45 +8,32 @@ using namespace std;
 struct SDLState {
 	SDL_Window* window;
 	SDL_Renderer* renderer;
+	int width, height, logW, logH;
 };
 
 void cleanup(SDLState &state);
-bool intialize(SDLState& state);
+bool initialize(SDLState& state);
 
 
 int main(int argc, char* argv[]) {
 //it needs this argc and argv as well as its pulling it from the command line
 //NB You can only have one main file in your project like this otherwise it gets a little confused :)
 	SDLState state;
-	if (!SDL_Init(SDL_INIT_VIDEO)) {
-		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", nullptr);
-		cleanup(state);
+	state.width = 1600;
+	state.height = 900;
+	state.logW = 640;
+	state.logH = 320;
+	if (!initialize(state)) {
 		return 1;
 	}
-	//create the window
-	int width = 800;
-	int height = 600;
-	state.window = SDL_CreateWindow("SDL3 practice", width, height, SDL_WINDOW_RESIZABLE);
-	if (!state.window) {
-		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", state.window);
-		cleanup(state);
-		return 1;
-	}
-	//create the Renderer
-	state.renderer = SDL_CreateRenderer(state.window, nullptr);
-	if (!state.renderer) {
-		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error Creating Renderer", state.window);
-		cleanup(state);
-		return 1;
-	}
-	//configure presentation
-	int logW = 640;
-	int logH = 320;
-	SDL_SetRenderLogicalPresentation(state.renderer, logW, logH, SDL_LOGICAL_PRESENTATION_LETTERBOX);
-
 	//load game assets
-	SDL_Texture* idleTex = IMG_LoadTexture(state.renderer, "data/idle.png");
+	SDL_Texture* idleTex = IMG_LoadTexture(state.renderer, "data/AnimationSheet_Character.png");
 	SDL_SetTextureScaleMode(idleTex, SDL_SCALEMODE_NEAREST);
+	//setup game data
+	const bool *keys = SDL_GetKeyboardState(nullptr);
+	float playerX = 150;
+	const float floor = state.logH;
+
 	//start the game loop
 	bool running = true;
 	while (running) {
@@ -58,20 +45,23 @@ int main(int argc, char* argv[]) {
 				running = false;
 				break;
 			case SDL_EVENT_WINDOW_RESIZED:
-				width = event.window.data1;
-				height = event.window.data2;
+				state.width = event.window.data1;
+				state.height = event.window.data2;
 				break;
 			}
 		}
+
+
+
 		//perform drawing
-		//starting simple this will draw the background white
-		SDL_SetRenderDrawColor(state.renderer, 20, 10, 30, 50);
+		SDL_SetRenderDrawColor(state.renderer, 20, 10, 30, 255);
 		SDL_RenderClear(state.renderer);
 		//so they were using intialiazers which i dont have not sure how to update to latest version of C++
 		//but x,y,width height are whats being used here
-		SDL_FRect src{0,0,128,128 };
-		SDL_FRect dst{ 0,0,128,128 };
-		SDL_RenderTexture(state.renderer, idleTex, &src, nullptr);
+		const int spriteSize = 32;
+		SDL_FRect src{ 0,0,spriteSize,spriteSize };
+		SDL_FRect dst{ playerX,floor - spriteSize ,spriteSize,spriteSize };
+		SDL_RenderTexture(state.renderer, idleTex, &src, &dst);
 		//swap buffer
 		SDL_RenderPresent(state.renderer);
 	}
@@ -84,4 +74,34 @@ void cleanup(SDLState &state) {
 	SDL_DestroyRenderer(state.renderer);
 	SDL_DestroyWindow(state.window);
 	SDL_Quit();
+}
+bool initialize(SDLState& state) {
+	bool initSuccess = true;
+	if (!SDL_Init(SDL_INIT_VIDEO)) {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", nullptr);
+		cleanup(state);
+		initSuccess = false;
+		return 1;
+	}
+	//create the window
+	
+	state.window = SDL_CreateWindow("SDL3 practice", state.width, state.height, SDL_WINDOW_RESIZABLE);
+	if (!state.window) {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", state.window);
+		cleanup(state);
+		initSuccess = false;
+		return 1;
+	}
+	//create the Renderer
+	state.renderer = SDL_CreateRenderer(state.window, nullptr);
+	if (!state.renderer) {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error Creating Renderer", state.window);
+		cleanup(state);
+		initSuccess = false;
+		return 1;
+	}
+	//configure presentation
+	
+	SDL_SetRenderLogicalPresentation(state.renderer, state.logW, state.logH, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+	return initSuccess;
 }
